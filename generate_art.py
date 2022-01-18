55f32df56374230ceb1732cf00c3c780edd300db
Eloka <12972250+elokaagu@users.noreply.github.com>
2022-01-18 15:53:11 +0000

    black background

--- generate_art.py
+++ generate_art.py
@@ -1,14 +1,24 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageChops
 import random
 
 def random_color():
     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255) )
 
+def interpolate(start_color, end_color, factor: float):
+    reciprocal = 1 - factor
+    return (
+        int(start_color[0] * reciprocal + end_color[0] * factor),
+        int(start_color[1] * reciprocal + end_color[1] * factor),
+        int(start_color[2] * reciprocal + end_color[2] * factor),
+    )
+
 def generate_art():
     print("Generating Art")
     image_size_px = 128
     padding_px = 16
-    image_bg_color = (255, 255, 255)
+    image_bg_color = (0, 0, 0)
+    start_color = random_color()
+    end_color = random_color()
 
 
     image = Image.new("RGB", 
@@ -21,8 +31,7 @@ def generate_art():
 
     points = []
 
-    # Geberate the points
-    thickness = 0
+    # Generate the points
     for _ in range(10):
         random_point = (
             random.randint(padding_px, image_size_px - padding_px),
@@ -30,19 +39,48 @@ def generate_art():
             )
         points.append(random_point)
 
-    # Draw the points
+
+    # Draw bounding box
+    min_x = min([p[0] for p in points])
+    max_x = max([p[0] for p in points])
+    min_y = min([p[1] for p in points])
+    max_y = max([p[1] for p in points])
+
+
+    # Center the image
+    delta_x = min_x - (image_size_px - max_x)
+    delta_y = min_y - (image_size_px - max_y)
+
     for i, point in enumerate(points):
+        points[i] = (point[0] - delta_x // 2, point[1] - delta_y // 2)
+
+
+    # Draw the points
+    thickness = 0
+    n_points = len(points) - 1
+
+    for i, point in enumerate(points):
+
+        # Overlay canvas        
+        overlay_image = Image.new("RGB", 
+        size=(image_size_px, image_size_px), 
+        color=(image_bg_color))
+
+        overlay_draw = ImageDraw.Draw(overlay_image)
+
         p1 = point
         
-        if i == len(points) - 1:
+        if i == n_points:
             p2 = points[0]
         else:
             p2 = points[i + 1]
 
         line_xy = (p1,p2)
-        line_color = random_color()
+        color_factor = i / n_points
+        line_color = interpolate(start_color, end_color, color_factor)
         thickness += 1
-        draw.line(line_xy, fill=line_color, width=thickness)
+        overlay_draw.line(line_xy, fill=line_color, width=thickness)
+        image = ImageChops.add(image, overlay_image)
         
     image.save("test_image.png")
 
