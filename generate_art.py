9c52d27dc6ee601251bba1eed486caf5b93e5e02
Eloka <12972250+elokaagu@users.noreply.github.com>
2022-01-18 14:55:14 +0000

    random lines

--- generate_art.py
+++ generate_art.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageDraw
+import random
 
 def generate_art():
     print("Generating Art")
@@ -9,6 +10,24 @@ def generate_art():
     image = Image.new("RGB", 
     size=(image_size_px, image_size_px), 
     color=(image_bg_color))
+
+
+    # Draw some lines.
+    draw = ImageDraw.Draw(image)
+
+    for _ in range(10):
+        random_point_1 = (
+            random.randint(0, image_size_px),
+            random.randint(0, image_size_px),
+            )
+        random_point_2 = (
+            random.randint(0, image_size_px), 
+            random.randint(0, image_size_px),
+            )
+        line_xy = (random_point_1,random_point_2)
+        line_color = ( 0, 0, 0)
+        draw.line(line_xy, fill=line_color)
+        
     image.save("test_image.png")
 
 if __name__ == "__main__":
